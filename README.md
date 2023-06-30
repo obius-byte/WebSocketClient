e85dfddcca9eddfc04488d85432f16e4bd2fbe69
obius-byte <18569827+obius-byte@users.noreply.github.com>
2023-07-01 00:15:20 +0300

    added minified version, update README

--- README.md
+++ README.md
@@ -5,73 +5,68 @@
 
 ### Local
 ```html
-<script src="/path-to-file/web-socket-client.js"></script>
+<script src="/ws-client.min.js"></script>
 ```
 
 ## Example
 
 ### Sending and receiving data in JSON format
 ```js
-var wsClient = new WebSocketClient();
-wsClient.useJSON(true);
+const ws = new WebSocketClient();
+ws.useJSON(true);
 
 // Instant connection
 /*(async () => {
-	await wsClient.connect('ws://127.0.0.1:8006')
-	wsClient.addEventListener('message', (e) => {
+	await ws.connect('ws://127.0.0.1:8006', 5000)
+	ws.addEventListener('message', (e) => {
 		const message = JSON.parse(e.data)
 		console.log('onmessage', message)
 	});
 })();*/
 
 async function connect() {
-	await wsClient.connect('ws://127.0.0.1:8006', 5000)
-	
-	wsClient.removeAllListener()
-	wsClient.addEventListener('message', (e) => {
+	await ws.connect('ws://127.0.0.1:8006', 5000)	
+	ws.removeAllListener()
+	ws.addEventListener('message', (e) => {
 		const message = JSON.parse(e.data)
 		console.log('onmessage', message)
 	});
 }
 
 function state() {
-	console.info('isConnecting: ' + wsClient.isConnecting())
-	console.info('isConnected: ' + wsClient.isConnected())
-	console.info('isClosing: ' + wsClient.isClosing())
-	console.info('isClosed: ' + wsClient.isClosed())
+	console.info('isConnecting: ' + ws.isConnecting())
+	console.info('isConnected: ' + ws.isConnected())
+	console.info('isClosing: ' + ws.isClosing())
+	console.info('isClosed: ' + ws.isClosed())
 }
 
 function send() {
-	wsClient.send({ name: 'selectFolder' });
+	ws.send({ name: 'selectFolder' });
 }
 
 function disconnect() {
-	wsClient.disconnect();
+	ws.disconnect();
 }
 ```
 
 ### Without JSON format
 ```js
-var wsClient = new WebSocketClient();
-wsClient.useJSON(false);
+const ws = new WebSocketClient();
+ws.useJSON(false);
 
 async function connect() {
-	await wsClient.connect('ws://127.0.0.1:8006', 5000)
-	
-	wsClient.removeAllListener()
-	wsClient.addEventListener('message', (e) => {
+	await ws.connect('ws://127.0.0.1:8006', 5000)	
+	ws.removeAllListener()
+	ws.addEventListener('message', (e) => {
 		const message = e.data
 		console.log('onmessage', message)
 	});
 }
 
-...
-
 function send() {
-	wsClient.send('selectFolder);
+	ws.send('selectFolder')
 }
 
-...
 ```
 
 ## License
---
+++ ws-client.js
@@ -0,0 +1,87 @@
+class WebSocketClient {
+    #_connection = null
+
+    #_events = []
+
+    #_useJSON = false
+
+    constructor() {
+        //
+    }
+
+    useJSON(value) {
+        this.#_useJSON = value
+    }
+
+    #_webSocketAsync(url) {
+        return new Promise((resolve, reject) => {
+            const ws = new WebSocket(url);
+            ws.onopen = () => { resolve(ws) }
+            ws.onerror = e => { reject(e) }
+        });
+    }
+
+    async connect(url, timeout = 3000) {
+        if (this.isConnecting() || this.isConnected() ) {
+            return
+        }
+
+        try {
+            this.#_connection = await this.#_webSocketAsync(url)
+            console.info('Websocket: %cConnected!', 'color: #28a745')
+            this.#_events.forEach(event => {
+                this.#_connection.addEventListener(event.type, event.callback)
+            })
+            this.#_connection.addEventListener('close', e => {
+                console.info('Websocket: %cConnection terminated!', 'color: #dc3545')
+                if (!e.wasClean) {
+                    console.info('Websocket: %cTrying to reconnect...', 'color: #ffc107')
+                    setTimeout(async () => await this.connect(url), timeout)
+                }
+            })
+        } catch (e) {
+            console.info('Websocket: %cTrying to connect...', 'color: #ffc107')
+            setTimeout(async () => await this.connect(url), timeout)
+        }
+    }
+
+    addEventListener(type, callback) {
+        this.#_events.push({type, callback})
+        this.#_connection?.addEventListener(type, callback)
+    }
+
+    removeAllListener() {
+        this.#_events.forEach(event => {
+            this.#_connection?.removeEventListener(event.type, event.callback)
+        })
+        this.#_events = []
+    }
+
+    send(data) {
+        if (this.#_useJSON) {
+            data = JSON.stringify(data)
+        }
+        this.#_connection?.send(data)
+    }
+
+    disconnect() {
+        this.#_connection?.close()
+        this.#_connection = null
+    }
+
+    isConnecting() {
+        return this.#_connection?.readyState === WebSocket.CONNECTING
+    }
+
+    isConnected() {
+        return this.#_connection?.readyState === WebSocket.OPEN
+    }
+
+    isClosing() {
+        return this.#_connection?.readyState === WebSocket.CLOSING
+    }
+
+    isClosed() {
+        return this.#_connection?.readyState === WebSocket.CLOSED
+    }
+}
